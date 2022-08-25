31b015b887d7f1bcb4374d9f0e5a36c0f46919c6
elliott-king <8884571+elliott-king@users.noreply.github.com>
2022-08-24 23:33:37 -0400

    Fix TS implementation

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import "./App.css";
 import mondaySdk from "monday-sdk-js";
 import "monday-ui-react-core/dist/main.css";
 //Explore more Monday React Components here: https://style.monday.com/
-import AttentionBox from "monday-ui-react-core/dist/AttentionBox.js";
+// @ts-ignore (no declared types)
+import { AttentionBox } from "monday-ui-react-core";
 
 const monday = mondaySdk();
 
-const App = ({}) => {
+const App = () => {
   const [settings, setSettings] = useState({});
   const [name, setName] = useState("");
 
@@ -25,31 +26,5 @@ const App = ({}) => {
     </div>
   );
 };
-
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-
-//     // Default state
-//     this.state = {
-//       settings: {},
-//       name: "",
-//     };
-//   }
-
-//   componentDidMount() {
-//     // TODO: set up event listeners
-//   }
-
-//   render() {
-//     return <div className="App">
-//       <AttentionBox
-//         title="Hello Monday Apps!"
-//         text="Ready to start my app journey by building a view!"
-//         type="success"
-//       />
-//     </div>;
-//   }
-// }
 
 export default App;
