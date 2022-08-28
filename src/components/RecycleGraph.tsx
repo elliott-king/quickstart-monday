45987b3ea8c6eaf1f0492d0fc1acb82a9be797cf
elliott-king <8884571+elliott-king@users.noreply.github.com>
2022-08-28 15:57:39 -0400

    Add state for recycle history graph

--- src/components/RecycleGraph.tsx
+++ src/components/RecycleGraph.tsx
@@ -1,7 +1,21 @@
 import React, { useState } from "react";
+import { DateTime } from "luxon";
+import mondaySdk from "monday-sdk-js";
 
 // @ts-ignore (no declared types)
 import { Button, Flex } from "monday-ui-react-core";
+import { useEffect } from "react";
+
+const DEFAULT_RECYCLABLES_COUNTS: Array<number> = Array(10).fill(0);
+
+declare global {
+  // Allows us to add debug method to global window handler
+  interface Window {
+    debugBackfill: any;
+  }
+}
+
+const monday = mondaySdk();
 
 const RemoveButton = (props: React.HTMLAttributes<SVGElement>) => (
   <svg
@@ -42,13 +56,93 @@ const AddButton = (props: React.HTMLAttributes<SVGElement>) => (
   </svg>
 );
 
+const currentDateStr = () => DateTime.utc().toISODate();
+
+const lastTenDaysAsIsoStrings = () => {
+  let date = DateTime.utc();
+  const dates: Array<string> = [];
+  for (let i = 0; i < 10; i++) {
+    dates.push(date.toISODate());
+    date = date.minus({ days: 1 });
+  }
+  return dates;
+};
+
+/** Debug method to backfill last ten days with random data */
+const debugBackfill = (
+  setRecycleCounts: React.Dispatch<React.SetStateAction<Array<number>>>
+) => {
+  const datestrs = lastTenDaysAsIsoStrings();
+  const max = 10;
+  const counts = [...DEFAULT_RECYCLABLES_COUNTS];
+  datestrs.forEach((dtstr, i) => {
+    const value = Math.floor(Math.random() * max);
+    monday.storage.instance.setItem(dtstr, value);
+    counts[i] = value;
+  });
+  setRecycleCounts(counts);
+};
+
 export const RecycleGraphPane = () => {
   const [recycleCount, setRecycleCount] = useState(0);
+  const [last10DaysRecycleCount, setRecycleCounts] = useState(
+    DEFAULT_RECYCLABLES_COUNTS
+  );
+
+  // On first render, fetch recycling history of this instance
+  // Note that this is only instance-level storage
+  // https://developer.monday.com/apps/docs/advanced-view#saving-persistent-data-using-the-mondaycom-storage-api
+  useEffect(() => {
+    const isoStrings = lastTenDaysAsIsoStrings();
+    // NOTE: getItem only works on a dashboard. Does not work locally.
+    const promises = isoStrings.map((dtstr) =>
+      monday.storage.instance.getItem(dtstr).then((res) => {
+        // console.log(dtstr);
+        // console.log(res.data);
+        return res.data?.value || 0;
+      })
+    );
+    Promise.all(promises).then(setRecycleCounts);
+  }, []);
+
+  useEffect(() => {
+    window.debugBackfill = () => debugBackfill(setRecycleCounts);
+    return () => (window.debugBackfill = undefined);
+  }, []);
+
   const reduceCount = () =>
     setRecycleCount((prev) => {
       if (prev <= 0) return prev;
       return prev - 1;
     });
+
+  const submitCount = () => {
+    // Update number in both states & also send to Monday API to store
+    setRecycleCount(() => {
+      setRecycleCounts((prev) => {
+        if (prev.length < 1) return prev;
+
+        // Make a copy to avoid directly mutating state
+        const copy = [...prev];
+        const totalToday = copy[copy.length - 1] + recycleCount;
+
+        // Update storage on API
+        const currentDate = currentDateStr();
+        monday.storage.instance.setItem(currentDate, totalToday);
+        // Used for debugging
+        // (res) => {
+        // console.log(res);
+        // monday.storage.instance.getItem(currentDate).then(console.log);
+        // });
+
+        // Update 10-day recycle count history
+        copy[copy.length - 1] = totalToday;
+        return copy;
+      });
+      // Update single-value shown as number
+      return 0;
+    });
+  };
   return (
     <Flex direction={Flex.directions.COLUMN} gap={Flex.gaps.SMALL}>
       <img src="graph-ss.png" alt="recycle graph" />
@@ -72,8 +166,7 @@ export const RecycleGraphPane = () => {
             style={{ cursor: "pointer", userSelect: "none" }}
           />
         </span>
-        {/* todo: actually do something with this */}
-        <Button onClick={() => setRecycleCount(0)}>Submit</Button>
+        <Button onClick={submitCount}>Submit</Button>
       </Flex>
     </Flex>
   );
