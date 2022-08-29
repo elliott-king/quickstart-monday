666c8a027ad1308fb61a146cedc1e6938ebfa232
elliott-king <8884571+elliott-king@users.noreply.github.com>
2022-08-29 07:59:54 -0400

    Add map widget

--- src/components/RecyclingMap.tsx
+++ src/components/RecyclingMap.tsx
@@ -3,9 +3,19 @@ import React from "react";
 // @ts-ignore (no declared types)
 import { Flex } from "monday-ui-react-core";
 
+const API_KEY = "AIzaSyDY0FxIMQhH_DDlNWH_-rVAcwUZY7uITYc";
+
 export const RecyclingMap = () => (
   <Flex direction={Flex.directions.COLUMN} style={{ alignItems: "center" }}>
     <h4>Recycling Locations Near You:</h4>
-    <img src="map-ss.png" alt="screenshot of a map" />
+    <iframe
+      width="325"
+      height="206"
+      style={{ border: 0 }}
+      title="recycling centers map"
+      loading="lazy"
+      allowFullScreen
+      src={`https://www.google.com/maps/embed/v1/search?q=recycling%20center&key=${API_KEY}`}
+    ></iframe>
   </Flex>
 );
