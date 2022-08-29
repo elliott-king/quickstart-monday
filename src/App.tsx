038da944b32c5b79378ad5989a30b8691ac6eeb5
elliott-king <8884571+elliott-king@users.noreply.github.com>
2022-08-29 09:14:06 -0400

    Move state to top-level component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { DateTime } from "luxon";
+import mondaySdk from "monday-sdk-js";
 import "./App.css";
 import "monday-ui-react-core/dist/main.css";
 //Explore more Monday React Components here: https://style.monday.com/
@@ -10,33 +12,161 @@ import { RecycleChecker } from "./components/RecycleChecker";
 import { RecyclingMap } from "./components/RecyclingMap";
 import { RecyclingRemainder } from "./components/RecyclingRemainder";
 
-const App = () => (
-  <div className="App">
-    <Box rounded={Box.roundeds.MEDIUM} border={Box.borders.DEFAULT}>
-      <Flex>
-        <Flex
-          direction={Flex.directions.COLUMN}
-          gap={Flex.gaps.LARGE}
-          style={{ padding: "30px" }}
-        >
-          <RecycleGraphPane />
-          <NotClippy />
-        </Flex>
-        <Flex
-          direction={Flex.directions.COLUMN}
-          style={{ padding: "30px", borderLeft: "black 2px solid" }}
-        >
-          {/* TODO: change favicon */}
-          {/* todo: search bar */}
-          <RecycleChecker />
-          <Flex gap={Flex.gaps.LARGE}>
-            <RecyclingMap />
-            <RecyclingRemainder />
+const DEFAULT_RECYCLABLES_COUNTS: Array<number> = Array(10).fill(0);
+
+declare global {
+  // Allows us to add debug method to global window handler
+  interface Window {
+    debugBackfill: any;
+  }
+}
+
+const monday = mondaySdk();
+
+const currentDateStr = () => DateTime.utc().toISODate();
+
+const lastTenDaysAsIsoStrings = () => {
+  let date = DateTime.utc();
+  const dates: Array<string> = [];
+  for (let i = 0; i < 10; i++) {
+    dates.unshift(date.toISODate());
+    date = date.minus({ days: 1 });
+  }
+  return dates;
+};
+
+/** Debug method to backfill last ten days with random data */
+const debugBackfill = (
+  setRecycleCounts: React.Dispatch<React.SetStateAction<Array<number>>>
+) => {
+  const datestrs = lastTenDaysAsIsoStrings();
+  const max = 10;
+  const counts = [...DEFAULT_RECYCLABLES_COUNTS];
+  datestrs.forEach((dtstr, i) => {
+    const value = Math.floor(Math.random() * max);
+    monday.storage.instance.setItem(dtstr, value);
+    counts[i] = value;
+  });
+  setRecycleCounts(counts);
+};
+
+const App = () => {
+  // State
+  const [recycleCount, setRecycleCount] = useState(0);
+  const [last10DaysRecycleCount, setRecycleCounts] = useState(
+    DEFAULT_RECYCLABLES_COUNTS
+  );
+
+  // Derived state
+  const totalCount = last10DaysRecycleCount.reduce(
+    (total, curr) => total + curr,
+    0
+  );
+  const totalStreak = last10DaysRecycleCount.reduce(
+    (total, curr) => (curr ? total + 1 : 0),
+    0
+  );
+
+  // On first render, fetch recycling history of this instance
+  // Note that this is only instance-level storage
+  // https://developer.monday.com/apps/docs/advanced-view#saving-persistent-data-using-the-mondaycom-storage-api
+  useEffect(() => {
+    const isoStrings = lastTenDaysAsIsoStrings();
+    // NOTE: getItem only works on a dashboard. Does not work locally.
+    const promises = isoStrings.map((dtstr) =>
+      monday.storage.instance.getItem(dtstr).then((res) => {
+        // console.log(dtstr);
+        // console.log(res.data);
+        return parseInt(res.data?.value || 0);
+      })
+    );
+    Promise.all(promises).then(setRecycleCounts);
+  }, []);
+
+  useEffect(() => {
+    window.debugBackfill = () => debugBackfill(setRecycleCounts);
+    return () => (window.debugBackfill = undefined);
+  }, []);
+
+  const reduceCount = () =>
+    setRecycleCount((prev) => {
+      if (prev <= 0) return prev;
+      return prev - 1;
+    });
+
+  const increaseCount = () => setRecycleCount(recycleCount + 1);
+
+  const submitCount = () => {
+    // Update number in both states & also send to Monday API to store
+    setRecycleCount(() => {
+      setRecycleCounts((prev) => {
+        if (prev.length < 1) return prev;
+
+        // Make a copy to avoid directly mutating state
+        const copy = [...prev];
+        const totalToday = copy[copy.length - 1] + recycleCount;
+
+        // Update storage on API
+        const currentDate = currentDateStr();
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const setPromise = monday.storage.instance.setItem(
+          currentDate,
+          totalToday
+        );
+
+        // Used for debugging
+        // setPromise.then((res) => {
+        //   console.log(res);
+        //   monday.storage.instance
+        //     .getItem(currentDate)
+        //     .then((res) => console.log(res.data));
+        // });
+
+        // Update 10-day recycle count history
+        copy[copy.length - 1] = totalToday;
+        return copy;
+      });
+      // Update single-value shown as number
+      return 0;
+    });
+  };
+
+  return (
+    <div className="App">
+      <Box rounded={Box.roundeds.MEDIUM} border={Box.borders.DEFAULT}>
+        <Flex>
+          <Flex
+            direction={Flex.directions.COLUMN}
+            gap={Flex.gaps.LARGE}
+            style={{ padding: "30px" }}
+          >
+            <RecycleGraphPane
+              count={recycleCount}
+              historycounts={last10DaysRecycleCount}
+              total={totalCount}
+              streak={totalStreak}
+              onReduceCount={reduceCount}
+              onIncreaseCount={increaseCount}
+              onSubmit={submitCount}
+            />
+            <NotClippy />
+          </Flex>
+          <Flex
+            direction={Flex.directions.COLUMN}
+            style={{ padding: "30px", borderLeft: "black 2px solid" }}
+          >
+            {/* TODO: change favicon */}
+            {/* todo: search bar */}
+            <RecycleChecker />
+            <Flex gap={Flex.gaps.LARGE}>
+              <RecyclingMap />
+              <RecyclingRemainder total={totalCount} />
+            </Flex>
           </Flex>
         </Flex>
-      </Flex>
-    </Box>
-  </div>
-);
+      </Box>
+    </div>
+  );
+};
 
 export default App;
--- src/components/RecycleGraph.tsx
+++ src/components/RecycleGraph.tsx
@@ -1,6 +1,4 @@
-import React, { useState } from "react";
-import { DateTime } from "luxon";
-import mondaySdk from "monday-sdk-js";
+import React from "react";
 
 import {
   Chart as ChartJS,
@@ -16,16 +14,6 @@ import { Line } from "react-chartjs-2";
 
 // @ts-ignore (no declared types)
 import { Button, Flex } from "monday-ui-react-core";
-import { useEffect } from "react";
-
-const DEFAULT_RECYCLABLES_COUNTS: Array<number> = Array(10).fill(0);
-
-declare global {
-  // Allows us to add debug method to global window handler
-  interface Window {
-    debugBackfill: any;
-  }
-}
 
 ChartJS.register(
   CategoryScale,
@@ -36,8 +24,6 @@ ChartJS.register(
   Tooltip,
   Legend
 );
-
-const monday = mondaySdk();
 
 const ChartContainer = ({
   counts,
@@ -162,112 +148,26 @@ const AddButton = (props: React.HTMLAttributes<SVGElement>) => (
   </svg>
 );
 
-const currentDateStr = () => DateTime.utc().toISODate();
-
-const lastTenDaysAsIsoStrings = () => {
-  let date = DateTime.utc();
-  const dates: Array<string> = [];
-  for (let i = 0; i < 10; i++) {
-    dates.push(date.toISODate());
-    date = date.minus({ days: 1 });
-  }
-  return dates;
-};
-
-/** Debug method to backfill last ten days with random data */
-const debugBackfill = (
-  setRecycleCounts: React.Dispatch<React.SetStateAction<Array<number>>>
-) => {
-  const datestrs = lastTenDaysAsIsoStrings();
-  const max = 10;
-  const counts = [...DEFAULT_RECYCLABLES_COUNTS];
-  datestrs.forEach((dtstr, i) => {
-    const value = Math.floor(Math.random() * max);
-    monday.storage.instance.setItem(dtstr, value);
-    counts[i] = value;
-  });
-  setRecycleCounts(counts);
-};
-
-export const RecycleGraphPane = () => {
-  // State
-  const [recycleCount, setRecycleCount] = useState(0);
-  const [last10DaysRecycleCount, setRecycleCounts] = useState(
-    DEFAULT_RECYCLABLES_COUNTS
-  );
-
-  // Derived state
-  const totalCount = last10DaysRecycleCount.reduce(
-    (total, curr) => total + curr,
-    0
-  );
-  const totalStreak = last10DaysRecycleCount.reduce(
-    (total, curr) => (curr ? total + 1 : 0),
-    0
-  );
-
-  // On first render, fetch recycling history of this instance
-  // Note that this is only instance-level storage
-  // https://developer.monday.com/apps/docs/advanced-view#saving-persistent-data-using-the-mondaycom-storage-api
-  useEffect(() => {
-    const isoStrings = lastTenDaysAsIsoStrings();
-    // NOTE: getItem only works on a dashboard. Does not work locally.
-    const promises = isoStrings.map((dtstr) =>
-      monday.storage.instance.getItem(dtstr).then((res) => {
-        // console.log(dtstr);
-        // console.log(res.data);
-        return parseInt(res.data?.value || 0);
-      })
-    );
-    Promise.all(promises).then(setRecycleCounts);
-  }, []);
-
-  useEffect(() => {
-    window.debugBackfill = () => debugBackfill(setRecycleCounts);
-    return () => (window.debugBackfill = undefined);
-  }, []);
-
-  const reduceCount = () =>
-    setRecycleCount((prev) => {
-      if (prev <= 0) return prev;
-      return prev - 1;
-    });
-
-  const submitCount = () => {
-    // Update number in both states & also send to Monday API to store
-    setRecycleCount(() => {
-      setRecycleCounts((prev) => {
-        if (prev.length < 1) return prev;
-
-        // Make a copy to avoid directly mutating state
-        const copy = [...prev];
-        const totalToday = copy[copy.length - 1] + recycleCount;
-
-        // Update storage on API
-        const currentDate = currentDateStr();
-        monday.storage.instance.setItem(currentDate, totalToday);
-        // Used for debugging
-        // (res) => {
-        // console.log(res);
-        // monday.storage.instance.getItem(currentDate).then(console.log);
-        // });
-
-        // Update 10-day recycle count history
-        copy[copy.length - 1] = totalToday;
-        return copy;
-      });
-      // Update single-value shown as number
-      return 0;
-    });
-  };
-
+export const RecycleGraphPane = ({
+  count,
+  historycounts,
+  total,
+  streak,
+  onReduceCount,
+  onIncreaseCount,
+  onSubmit,
+}: {
+  count: number;
+  historycounts: Array<number>;
+  total: number;
+  streak: number;
+  onReduceCount: () => void;
+  onIncreaseCount: () => void;
+  onSubmit: () => void;
+}) => {
   return (
     <Flex direction={Flex.directions.COLUMN} gap={Flex.gaps.SMALL}>
-      <ChartContainer
-        counts={last10DaysRecycleCount}
-        total={totalCount}
-        streak={totalStreak}
-      />
+      <ChartContainer counts={historycounts} total={total} streak={streak} />
       <Flex gap={Flex.gaps.SMALL}>
         <div>
           <div>
@@ -279,16 +179,16 @@ export const RecycleGraphPane = () => {
         </div>
         <span style={{ display: "inline-flex", alignItems: "center" }}>
           <RemoveButton
-            onClick={reduceCount}
+            onClick={onReduceCount}
             style={{ cursor: "pointer", userSelect: "none" }}
           />
-          &nbsp;{recycleCount}&nbsp;
+          &nbsp;{count}&nbsp;
           <AddButton
-            onClick={() => setRecycleCount(recycleCount + 1)}
+            onClick={onIncreaseCount}
             style={{ cursor: "pointer", userSelect: "none" }}
           />
         </span>
-        <Button color={Button.colors.POSITIVE} onClick={submitCount}>
+        <Button color={Button.colors.POSITIVE} onClick={onSubmit}>
           Submit
         </Button>
       </Flex>
