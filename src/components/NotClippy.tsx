ca6c999e4b7e7a90255921ac9f5503feac7ced33
elliott-king <8884571+elliott-king@users.noreply.github.com>
2022-09-06 08:00:21 -0400

    Replace hand-drawn image with photoshopped

--- src/components/NotClippy.tsx
+++ src/components/NotClippy.tsx
@@ -5,7 +5,7 @@ import { Flex } from "monday-ui-react-core";
 
 export const NotClippy = () => (
   <Flex>
-    <img src="/not-clippy.png" alt="hand-drawn crumpled plastic bottle" />
+    <img src="/not-clippy-2.png" alt="hand-drawn crumpled plastic bottle" />
     <img
       src="/not-clippy-speech-bubble.png"
       alt="Plastic bags can take up to 1000 years to decompose"
